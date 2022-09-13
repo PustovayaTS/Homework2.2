4ffdc5ff95611c23c70234fac4ec5adf4d17b9f7
PustovayaTS <ais_ta@bk.ru>
2022-09-13 13:48:34 +0700

    Задание 3 доделано

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int year = 2021;
-        if (year % 4 > 0) {
+        if (year % 4 != 0 || year % 100 == 0 && year % 400 != 0) {
             System.out.println(year + " год не является високосным");
         }
         else {
